28fb99e5eef8c2bbaecc34157e45d54d2af2edea
Scott Zhu <zhuhuihuihui@gmail.com>
2016-03-15 12:35:46 -0700

    finish project

--- Podfile
+++ Podfile
@@ -5,3 +5,4 @@ use_frameworks!
 pod 'Fabric'
 pod 'TwitterKit'
 pod 'TwitterCore'
+pod 'INSPullToRefresh'
--- TwitterDemo/ViewControllers/SearchResultViewController.m
+++ TwitterDemo/ViewControllers/SearchResultViewController.m
@@ -7,11 +7,16 @@
 //
 
 #import "SearchResultViewController.h"
+#import "UIScrollView+INSPullToRefresh.h"
+#import "INSTwitterPullToRefresh.h"
+#import "INSDefaultInfiniteIndicator.h"
 
-static NSString * const TweetTableReuseIdentifier = @"TweetCell";
+#define TweetTableReuseIdentifier @"TweetCell"
+#define SearchKeyword @"@peek"
+#define TwitterSearchEndPoint @"https://api.twitter.com/1.1/search/tweets.json"
 
 @interface SearchResultViewController ()
-@property (nonatomic, strong) NSArray *tweets; // Hold all the loaded tweets
+@property (nonatomic, strong) NSMutableArray *tweets; // Hold all the loaded tweets
 @end
 
 @implementation SearchResultViewController
@@ -20,24 +25,63 @@ static NSString * const TweetTableReuseIdentifier = @"TweetCell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSArray *tweetIDs = @[@"20", // @jack's first Tweet
-                          @"510908133917487104" // our favorite Bike tweet
-                          ];
-    
-    // Setup tableview
+    //1. Setup tableview
     self.tableView.estimatedRowHeight = 150;
     self.tableView.rowHeight = UITableViewAutomaticDimension;
     self.tableView.allowsSelection = NO;
     [self.tableView registerClass:[TWTRTweetTableViewCell class] forCellReuseIdentifier:TweetTableReuseIdentifier];
+    self.tableView.tableFooterView = [[UIView alloc] init];
     
-    // Load tweets
-    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
-    [client loadTweetsWithIDs:tweetIDs completion:^(NSArray *tweets, NSError *error) {
-        if (tweets) {
-            self.tweets = tweets;
+    //2. Setup pull to refresh component
+    [self.tableView ins_addPullToRefreshWithHeight:60.0 handler:^(UIScrollView *scrollView) {
+        int64_t delayInSeconds = 1;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self refreshTweetsWithMaxTweetID:nil completion:^(NSArray *tweets, NSError *error) {
+                if (nil == error) {
+                    self.tweets = [tweets mutableCopy];
+                    [self.tableView reloadData];
+                } else {
+                    NSLog(@"Error: %@", error);
+                }
+                [scrollView ins_endPullToRefresh];
+            }];
+        });
+    }];
+    CGRect pullFrame = CGRectMake(0, 0, 24, 24);
+    UIView <INSPullToRefreshBackgroundViewDelegate> *pullToRefresh = [[INSTwitterPullToRefresh alloc] initWithFrame:pullFrame];
+    self.tableView.ins_pullToRefreshBackgroundView.delegate = pullToRefresh;
+    [self.tableView.ins_pullToRefreshBackgroundView addSubview:pullToRefresh];
+    
+    //3. Setup infinity scrolling componet
+    [self.tableView ins_addInfinityScrollWithHeight:60.0 handler:^(UIScrollView *scrollView) {
+        int64_t delayInSeconds = 1;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self refreshTweetsWithMaxTweetID:  ((TWTRTweet *)self.tweets.lastObject).tweetID completion:^(NSArray *tweets, NSError *error) {
+                if (nil == error) {
+                    NSLog(@"loaded Tweets count = %lu", (unsigned long)tweets.count);
+                    [self.tweets addObjectsFromArray:[tweets subarrayWithRange:NSMakeRange(1, tweets.count - 1)]];
+                    [self.tableView reloadData];
+                } else {
+                    NSLog(@"Error: %@", error);
+                }
+                [scrollView ins_endInfinityScrollWithStoppingContentOffset:NO];
+            }];
+        });
+    }];
+    CGRect infinityFrame = CGRectMake(0, 0, 24, 24);
+    UIView <INSAnimatable> *infinityRefresh = [[INSDefaultInfiniteIndicator alloc] initWithFrame:infinityFrame];
+    [self.tableView.ins_infiniteScrollBackgroundView addSubview:infinityRefresh];
+    [infinityRefresh startAnimating];
+    
+    //4. Refresh tweets for the first time.
+    [self refreshTweetsWithMaxTweetID:nil completion:^(NSArray *tweets, NSError *error) {
+        if (nil == error) {
+            self.tweets = [tweets mutableCopy];
             [self.tableView reloadData];
         } else {
-            NSLog(@"Failed to load tweet: %@", [error localizedDescription]);
+            NSLog(@"Error: %@", error);
         }
     }];
 }
@@ -47,12 +91,55 @@ static NSString * const TweetTableReuseIdentifier = @"TweetCell";
     // Dispose of any resources that can be recreated.
 }
 
+#pragma -mark- Custom Methods
+- (void) refreshTweetsWithMaxTweetID: (NSString *) maxTweetID
+                          completion: (void (^)(NSArray *tweets, NSError *error)) completion {
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
+    NSDictionary *params = @{@"q": SearchKeyword, @"result_type": @"recent", @"max_id": maxTweetID? maxTweetID: @""};
+    NSError *clientError;
+    
+    NSURLRequest *request = [[[Twitter sharedInstance] APIClient] URLRequestWithMethod:@"GET" URL:TwitterSearchEndPoint parameters:params error:&clientError];
+    
+    if (request) {
+        [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            if (data) {
+                // handle the response data e.g.
+                NSError *jsonError;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                if (json && jsonError == nil) {
+                    NSArray *resultTweets = [json objectForKey:@"statuses"];
+                    if (resultTweets && [resultTweets count] > 0) {
+                        completion([TWTRTweet tweetsWithJSONArray:resultTweets], nil);
+                    } else {
+                        completion([[NSArray alloc] init], nil);
+                    }
+                } else {
+                    completion(nil, jsonError);
+                }
+            }
+            else {
+                completion(nil, connectionError);
+            }
+        }];
+    }
+    else {
+        NSLog(@"Error: %@", clientError);
+    }
+}
+
 #pragma -mark- UITableViewDataSource
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     TWTRTweet *tweet = self.tweets[indexPath.row];
     
     TWTRTweetTableViewCell *cell = (TWTRTweetTableViewCell *)[tableView dequeueReusableCellWithIdentifier:TweetTableReuseIdentifier forIndexPath:indexPath];
+    cell.tweetView.showActionButtons = YES;
     [cell configureWithTweet:tweet];
+    if (indexPath.row % 2 == 0) {
+        cell.tweetView.theme = TWTRTweetViewThemeLight;
+    } else {
+        cell.tweetView.theme = TWTRTweetViewThemeDark;
+    }
+    
     cell.tweetView.delegate = self;
     
     return cell;
@@ -70,10 +157,20 @@ static NSString * const TweetTableReuseIdentifier = @"TweetCell";
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     TWTRTweet *tweet = self.tweets[indexPath.row];
     
-    return [TWTRTweetTableViewCell heightForTweet:tweet width:CGRectGetWidth(self.view.bounds)];
+//    return [TWTRTweetTableViewCell heightForTweet:tweet width:CGRectGetWidth(self.view.bounds)];
+    return [TWTRTweetTableViewCell heightForTweet:tweet width:CGRectGetWidth(self.view.bounds) showingActions:YES];
 }
 
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
 
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.tweets removeObjectAtIndex:indexPath.row];
+        [self.tableView reloadData];
+    }
+}
 #pragma -mark- UITableViewDelegate
 
 @end
